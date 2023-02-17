8dfdb499c2569d2387ff219dd424580a7d24df1b
IbrahimSadek796 <118129618+IbrahimSadek796@users.noreply.github.com>
2023-02-17 14:21:17 +0200

    done
    js

---
+++ asset/js/index.js
@@ -0,0 +1,37 @@
+let btnUp = document.getElementById('btnUp');
+$('#btnUp').click(function(){
+    window.scrollTo(0,0)
+})
+
+
+
+
+
+
+
+
+// for(var i = 5;i<15;i++){
+//     setTimeout(()=>console.log(i),0)
+// }
+
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var b='b'
+
+// function a() {
+//     console.log('a');
+// }
+// var c=function c() {
+//     console.log('c');
+// }
+
+const x='2'+1
+const y = 2+true
+const l='2'-1
+const z = 'x'*1
+console.log(x,y,l,z);
